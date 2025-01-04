d1357cccec3a97935cca97070d81f525375088fb
Smit Patel <87602550+SchmittTriggerr@users.noreply.github.com>
2025-01-05 03:16:29 +0530

    nav-bar mobile completed

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function toggleMenu () {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+// Toggle menu when clicked on Hamburger
+// Toggle menu to inactive when clicked on menu links
 
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+  
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    
 }
+
+
+// Toggle menu to inactive when clicked outside menu box
+document.addEventListener('click', function(event) {
+    const navMenu = document.getElementById('nav-menu');
+    const hamburger = document.getElementById('hamburger');
+
+    // If the click is outside the nav menu and the hamburger icon, close the menu
+    if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+        navMenu.classList.remove('active');  // Hide the menu
+        hamburger.classList.remove('active'); // Reset hamburger icon to default state
+    }
+});
+  
